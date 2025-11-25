2e64624d3cba6363eea6eaa0bb5e5f5c54dce9c0
Earl Francis Villarias <earlvillariaswork@gmail.com>
2025-11-26 00:34:37 +0800

    disable es-lint

--- resources/js/types/window.d.ts
+++ resources/js/types/window.d.ts
@@ -2,12 +2,14 @@ export {};
 
 declare global {
     interface Window {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         axios: any;
     }
 }
 
 declare module '@inertiajs/react' {
     interface PageProps {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         [key: string]: any;
     }
 }
