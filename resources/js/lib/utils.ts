d5dfa175132628a034887321ca11cda052b37556
Earl Francis Villarias <earlvillariaswork@gmail.com>
2025-12-01 04:26:06 +0800

    feat: refactor user table + model, and added an util for title case

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function TitleCase(val: string) {
+    return val
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+        .join(' ');
+}
