7ba5705db952a56394362b44480b500e157af680
erlFn <earlvillarias@gmail.com>
2025-12-02 18:35:18 +0800

    feat: add data metrics in admin dashboard

--- resources/js/pages/Admin/dashboard.tsx
+++ resources/js/pages/Admin/dashboard.tsx
@@ -1,6 +1,9 @@
+import { DataMetrics } from "@/components/Admin/dashboard/data-metrics";
+import { Separator } from "@/components/ui/separator";
 import AppLayout from "@/layouts/app-layout";
 import { welcome } from "@/routes";
 import { BreadcrumbItem } from "@/types";
+import { HardDrive } from "lucide-react";
 
 export default function Dashboard() {
     const breadCrumbs: BreadcrumbItem[] = [
@@ -14,9 +17,22 @@ export default function Dashboard() {
         <AppLayout
             breadcrumbs={breadCrumbs}
         >
-            <p>
-                Admin Dashboard
-            </p>
+            <div className="w-full flex flex-col gap-2">
+                <span className="flex items-center gap-2 text-muted-foreground text-xs">
+                    <HardDrive
+                        className="size-4"
+                    />
+                    <p>
+                        Data Metrics
+                    </p>
+                </span>
+                <div className="grid grid-cols-2 gap-4">
+                    <DataMetrics/>
+                </div>
+            </div>
+            <Separator
+                className="my-4"
+            />
         </AppLayout>
     );
 }
