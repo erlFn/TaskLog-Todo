6f348fd8bb4117a2c035d8d8b20a9f78eeebab6a
Earl Francis Villarias <earlvillariaswork@gmail.com>
2025-12-05 01:51:17 +0800

    feat: add label field + render skeleton

--- resources/js/pages/Admin/task/index.tsx
+++ resources/js/pages/Admin/task/index.tsx
@@ -1,6 +1,9 @@
+import { LabelField } from "@/components/Common/label-field";
+import { RenderSkeleton } from "@/components/Common/render-skeleton";
 import AppLayout from "@/layouts/app-layout";
 import admin from "@/routes/admin";
 import { BreadcrumbItem } from "@/types";
+import { Clipboard } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     { title: 'Tasks', href: admin.tasks.url() }
@@ -11,9 +14,21 @@ export default function Index() {
         <AppLayout
             breadcrumbs={breadcrumbs}
         >
-            <p>
-                Admin Task Page
-            </p>
+            <div className="space-y-4">
+                <LabelField
+                    icon={Clipboard}
+                    label="Tasks Overview"
+                    hasSep={true}
+                    isLast={true}
+                >
+                    <div className="grid grid-cols-5 gap-4">
+                        <RenderSkeleton
+                            count={18}
+                            hasThin={true}
+                        />
+                    </div>
+                </LabelField>
+            </div>
         </AppLayout>
     );
 }
