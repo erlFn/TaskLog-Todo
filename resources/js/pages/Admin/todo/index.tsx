cf4d9990cf1dfae3f1aeaa90cfacf853c6e690c5
Earl Francis Villarias <earlvillariaswork@gmail.com>
2025-12-05 01:53:30 +0800

    feat: add label field + render skeleton in Admin Todo Page

--- resources/js/pages/Admin/todo/index.tsx
+++ resources/js/pages/Admin/todo/index.tsx
@@ -1,6 +1,9 @@
+import { LabelField } from "@/components/Common/label-field";
+import { RenderSkeleton } from "@/components/Common/render-skeleton";
 import AppLayout from "@/layouts/app-layout";
 import admin from "@/routes/admin";
 import { BreadcrumbItem } from "@/types";
+import { ListTodo } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     { title: 'ToDo', href: admin.todo.url() }
@@ -11,9 +14,21 @@ export default function Index() {
         <AppLayout
             breadcrumbs={breadcrumbs}
         >
-            <p>
-                Admin Todo Page
-            </p>
+            <div className="space-y-4">
+                <LabelField
+                    icon={ListTodo}
+                    label="To Do Overview"
+                    isLast={true}
+                    hasSep={true}
+                >
+                    <div className="grid grid-cols-5 gap-4">
+                        <RenderSkeleton
+                            count={12}
+                            hasThin={true}
+                        />
+                    </div>
+                </LabelField>
+            </div>
         </AppLayout>
     );
 }
