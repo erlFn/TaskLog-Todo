647705c6ca7aa4bb81cac43b32e8be398760af88
Earl Francis Villarias <earlvillariaswork@gmail.com>
2025-12-06 21:44:37 +0800

    feat: add task type interface

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -45,3 +45,13 @@ export interface TabsData {
     value: string;
     color?: string;
 }
+
+export interface Task {
+    id: number;
+    title: string;
+    description: string;
+    status: string;
+    priority: string;
+    created_at: string;
+    updated_at: string;
+}
