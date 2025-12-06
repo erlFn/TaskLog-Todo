f4077534b7f2da5da6006c595645270da062780f
Earl Francis Villarias <earlvillariaswork@gmail.com>
2025-12-06 21:15:07 +0800

    fix: user and admin routes

--- routes/admin.php
+++ routes/admin.php
@@ -2,13 +2,16 @@
 
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\TaskController;
+use App\Http\Controllers\Admin\ToDoController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('admin')->prefix('admin')->group(function () {
+Route::middleware('admin')->prefix('admin')->name('admin.')->group(function () {
 
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-    Route::get('/tasks', [TaskController::class, 'index'])->name('admin.tasks');
-    Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('admin.tasks.update');
-    Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('admin.tasks.destroy');
+    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks');
+    Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+
+    Route::get('/todo', [ToDoController::class, 'index'])->name('todo');
 });
--- routes/user.php
+++ routes/user.php
@@ -5,13 +5,13 @@ use App\Http\Controllers\User\TaskController;
 use App\Http\Controllers\User\ToDoController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('user')->prefix('user')->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('user.dashboard');
+Route::middleware('user')->name('user.')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     
-    Route::get('/tasks', [TaskController::class, 'index'])->name('user.tasks.index');
-    Route::post('/tasks', [TaskController::class, 'store'])->name('user.tasks.store');
-    Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('user.tasks.update');
-    Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('user.tasks.destroy');
+    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+    Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+    Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
     
     Route::get('/todo', [ToDoController::class, 'index'])->name('user.todo');
 });
