03b95b4a3b49b23d93c92540b536a717c54ed3b3
Earl Francis Villarias <earlvillariaswork@gmail.com>
2025-12-08 20:55:57 +0800

    fix: loading across all pages feature

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,6 +4,7 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import { Toaster } from 'sonner';
+import { LoadingProvider } from "@/hooks/use-loading";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -14,13 +15,13 @@ createInertiaApp({
         const root = createRoot(el);
 
         root.render(
-            <>
+            <LoadingProvider>
                 <App {...props} />
                 <Toaster
                     richColors
                     position='top-center'
                 />
-            </>
+            </LoadingProvider>
         );
     },
     progress: {
