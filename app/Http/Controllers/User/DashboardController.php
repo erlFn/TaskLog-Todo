ddc479b7f849cff322bb63df01e3d6f888255677
Earl Francis Villarias <earlvillariaswork@gmail.com>
2025-12-10 14:45:40 +0800

    implement paginated todos in dashboard user page

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\Task;
+use App\Models\Todo;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
@@ -20,9 +21,19 @@ class DashboardController extends Controller
             ->ownedBy(Auth::id())
             ->count();
 
+        $todos = Todo::query()
+            ->ownedBy(Auth::id())
+            ->simplePaginate(15);
+
+        $todosCount = Todo::query()
+            ->ownedBy(Auth::id())
+            ->count();
+
         return Inertia::render('User/dashboard', [
             'tasks' => $tasks,
             'taskCount' => $taskCount,
+            'todos' => $todos,
+            'todosCount' => $todosCount,
         ]);
     }
 }
--- resources/js/pages/User/dashboard.tsx
+++ resources/js/pages/User/dashboard.tsx
@@ -1,23 +1,32 @@
 import { DataMetrics } from "@/components/Admin/dashboard/data-metrics";
 import { DataDialog } from "@/components/Common/data-dialog";
 import { LabelField } from "@/components/Common/label-field";
-import { RenderSkeleton } from "@/components/Common/render-skeleton";
+import { Badge } from "@/components/ui/badge";
 import AppLayout from "@/layouts/app-layout";
 import user from "@/routes/user";
-import { BreadcrumbItem, Task } from "@/types";
-import { HardDriveDownload, Clipboard, LayoutList } from 'lucide-react'
+import { BreadcrumbItem, Task, Todo } from "@/types";
+import { router } from "@inertiajs/react";
+import { HardDriveDownload, Clipboard, LayoutList, ListTodo } from 'lucide-react'
 
 interface ContentProps {
     tasks: {
         data: Task[];
     };
     taskCount: number;
+    todos: {
+        data: Todo[];
+    };
+    todosCount: number;
 }
 
-export default function Dashboard({ tasks, taskCount } : ContentProps) {
+export default function Dashboard({ tasks, taskCount, todos, todosCount } : ContentProps) {
     const breadcrumbs: BreadcrumbItem[] = [
         { title: 'Dashboard', href: user.dashboard.url()}
     ];
+
+    const handleRedirect = (todo: Todo) => {
+        router.get(user.todo.view(todo));
+    };
 
     return(
         <AppLayout
@@ -54,16 +63,36 @@ export default function Dashboard({ tasks, taskCount } : ContentProps) {
                 <LabelField
                     icon={LayoutList}
                     label="To do"
-                    count={1}
+                    count={todosCount}
                     showAll={true}
                     href={user.todo.url()}
                     isLast={true}
                 >
                     <div className="grid grid-cols-5 gap-2">
-                        <RenderSkeleton
-                            count={10}
-                        />
-                    </div>
+                        {todos.data.map(todo => (
+                            <div
+                                onClick={() => handleRedirect(todo)}
+                                className="border p-4 rounded-md hover:border-blue-400/20 hover:bg-blue-400/4 transition-all duration-250 space-y-4 cursor-pointer"
+                            >
+                                <p className="text-neutral-700">
+                                    {todo.title}
+                                </p>
+                                <div className="flex flex-col gap-2">
+                                    <span className="flex items-center gap-1 text-xs text-muted-foreground">
+                                        <ListTodo
+                                            className="size-4"
+                                        />
+                                        <p>Total List</p>
+                                    </span>
+                                    <Badge
+                                        className="bg-blue-400"
+                                    >
+                                        {todo.list_count}
+                                    </Badge>
+                                </div>
+                            </div>
+                        ))}
+                </div>
                 </LabelField>
             </div>
         </AppLayout>
